11935e212bd2cf58f27feb191c049390285997d9
Lukas Trumm <lukas.trumm@gmail.com>
2024-03-05 09:00:45 +0100

    Refactor to plugin structure more aligned with cmp docs

--- lua/cmp_css_variables/init.lua
+++ lua/cmp_css_variables/init.lua
@@ -1,41 +1,47 @@
-local source = {}
 local utils = require("cmp_css_variables.utils")
 
-local opts = {
-	files = {},
+local Source = {
+	config = {
+		files = {},
+		filetypes = { "css", "vue" },
+	},
+	cache = {},
+	filetypes = {} -- a set of filetypes
 }
 
-source.new = function()
-	local self = setmetatable({}, { __index = source })
-	self.cache = {}
+Source.new = function(overrides)
+	local self = setmetatable({}, { __index = Source })
+	self.config = vim.tbl_extend("force", Source.config, overrides or {})
+	for _, item in ipairs(self.config.filetypes) do
+		self.filetypes[item] = true
+	end
 	return self
 end
 
-function source.is_available()
-	return vim.g.css_variables_files
-			and (
-				vim.bo.filetype == "scss"
-				or vim.bo.filetype == "sass"
-				or vim.bo.filetype == "css"
-				or vim.bo.filetype == "less"
-				or vim.bo.filetype == "vue"
-			)
+
+function Source:is_available()
+	if self.config.files then
+		if self.filetypes["*"] ~= nil or self.filetypes[vim.bo.filetype] ~= nil then
+			return true
+		end
+	end
+	return false
 end
 
-function source.get_debug_name()
+function Source:get_debug_name()
 	return "css_variables"
 end
 
-function source.get_trigger_characters()
+function Source:get_trigger_characters()
 	return { "-" }
 end
 
-function source.complete(self, _, callback)
+function Source:complete(_, callback)
 	local bufnr = vim.api.nvim_get_current_buf()
 	local items = {}
 
 	if not self.cache[bufnr] then
-		items = utils.get_css_variables(opts.files)
+		items = utils.get_css_variables(self.config.files)
 
 		if type(items) ~= "table" then
 			return callback()
@@ -48,20 +54,16 @@ function source.complete(self, _, callback)
 	callback({ items = items or {}, isIncomplete = false })
 end
 
-function source.resolve(_, completion_item, callback)
+function Source:resolve(completion_item, callback)
 	callback(completion_item)
 end
 
-function source.execute(_, completion_item, callback)
+function Source:execute(completion_item, callback)
 	callback(completion_item)
 end
-
-require("cmp").register_source("css_variables", source.new())
 
 return {
-	setup = function(_opts)
-		if _opts then
-			opts = vim.tbl_deep_extend("force", opts, _opts) -- will extend the default options
-		end
-	end,
+	setup = function(overrides)
+		require("cmp").register_source("css_variables", Source.new(overrides))
+	end
 }
